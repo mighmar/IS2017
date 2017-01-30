2d2e5253c47d8e97c72dbe1ddef6d6885d6290c1
Marko Crnobrnja <fdsafk@jiaofga.com>
2017-01-31 00:28:56 +0100

    dasdasdasd

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rm *.out *.toc *.aux *.dvi main.pdf
+rm *.out *.toc *.aux *.dvi *.bbl *.blg *.log main.pdf
 pdflatex -synctex=1 -interaction=nonstopmode main.tex
-rm *.out *.toc *.aux *.dvi
+rm *.out *.toc *.aux *.dvi *.bbl *.blg *.log
